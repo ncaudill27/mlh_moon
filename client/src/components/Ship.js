5b8f6387ced0f84db54209ef73379c978c239240
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-31 18:38:32 -0400

    Add acceleration to ship

--- client/src/components/Ship.js
+++ client/src/components/Ship.js
@@ -2,35 +2,56 @@ import React, { Component } from 'react';
 import ship from '../ship.png';
 
 const seedOrientation = () => getComputedStyle(document.documentElement)
-                        .getPropertyValue('--orientation')
-                        .slice(1,3);
+                              .getPropertyValue('--orientation')
+                              .slice(1,3);
 
+// establish 
 class Ship extends Component {
 
   state = {
-    orientation: seedOrientation()
+    orientation: seedOrientation(),
+    top: document.documentElement.clientTop,
+    left: document.documentElement.clientLeft
   }
   
   rotateShip = (deg) => {
     document.documentElement.style.setProperty('--orientation', deg);
   }
 
-  handleRotate = e => {
+  accelerateShip = () => {
+    let {top, left} = this.state;
+    window.scroll({top: top += 90, left: left += 90, behavior: 'smooth'});
+    this.setState({top, left});
+  }
+
+  handleMovement = e => {
     let orientation = parseInt(this.state.orientation, 10);
     if (e.keyCode === 65) {
-      orientation -= 2
+      orientation = this.maintainOrientation(orientation - 2);
       this.rotateShip( orientation + 'deg' );
       this.setState({orientation})
     } else if (e.keyCode === 68) {
-      orientation += 2
+      orientation = this.maintainOrientation(orientation + 2);
       this.rotateShip( orientation + 'deg' );
       this.setState({orientation})
-    };
+    } else if (e.keyCode === 87) {
+      this.accelerateShip();
+    }
+  }
+
+  maintainOrientation = orientation => {
+    if (orientation > 359) {
+      orientation = 0;
+    } else if (orientation < 0) {
+      orientation = 358;
+    }
+    return orientation;
   }
 
   render() {
+    console.log(this.state);
     return (
-      <div tabIndex='0' onKeyDown={ e => this.handleRotate(e) }>
+      <div tabIndex='0' onKeyDown={this.handleMovement}>
         <img
           className='Ship'
           src={ship}

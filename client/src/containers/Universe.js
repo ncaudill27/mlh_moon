1337e1b822812eac31f8fd08e34e4efe385e6259
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-31 20:12:02 -0400

    Generate planet array

--- client/src/containers/Universe.js
+++ client/src/containers/Universe.js
@@ -10,8 +10,12 @@ class Universe extends Component {
     // Creating the array elements for Planets
 
     state={
-        // planetsArray:[],
+        planetsArray:[],
         shipBoundaries: {}
+    }
+
+    componentDidMount() {
+      this.generatePlanets(this.createPlanet, 4);
     }
 
     findShip = shipBoundaries => {
@@ -20,22 +24,27 @@ class Universe extends Component {
       });
     }
 
-    // createPlanet=()=>{
-    //     const planet={
-    //         water: Math.floor(Math.random() * 255) + 1,
-    //         medicine: Math.floor(Math.random() * 255) + 1,
-    //         food: Math.floor(Math.random() * 255) + 1,
-    //         fuel: Math.floor(Math.random() * 255) + 1,
-    //         orbit: randomNumber(100, 400),
-    //         id: this.state.planetsArray.length - 1 
-    //     }
-    //     this.setState({planetsArray: [...this.state.planetsArray, planet] })
-    // }
+    createPlanet=()=>{
+        const planet={
+            water: Math.floor(Math.random() * 255) + 1,
+            medicine: Math.floor(Math.random() * 255) + 1,
+            food: Math.floor(Math.random() * 255) + 1,
+            fuel: Math.floor(Math.random() * 255) + 1,
+            orbit: randomNumber(400, 1000),
+            size: randomNumber(100, 400),
+            id: this.state.planetsArray.length ? this.state.planetsArray.length - 1 : null
+        }
+        this.setState( prevState => ({
+          planetsArray: [...prevState.planetsArray, planet]
+        }))
+    }
 
-    // generatePlanets(func, times) {
-    //     func();
-    //     times && --times && repeat(func, times);
-    // }
+    generatePlanets = (func, times) => {
+      console.log(times);
+      if (times === 0) return;
+      func();
+      return this.generatePlanets(func, --times);
+    }
 
 
     // Rendering Planet

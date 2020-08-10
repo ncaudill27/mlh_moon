4e6938404a49bf820f6c696e431decd86af46505
Nelson Caudill <nelson.a.caudill@gmail.com>
2020-08-10 13:16:44 -0400

    Style planets

--- client/src/components/Planet.js
+++ client/src/components/Planet.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import clouds from '../clouds.jpg';
 
 function Planet({water, medicine, food, top, left, size, id}) {
 
 	const planetStyle = {
-		backgroundColor: `rgb(${medicine}, ${food}, ${water})`,
+		backgroundColor: 'black',
 		width: `${size}px`,
 		height: `${size}px`,
 		position: 'absolute',
 		top: `${top}px`,
-		left: `${left}px`
+		left: `${left}px`,
+		background: `radial-gradient(circle at 100px 100px, rgb(${medicine}, ${food}, ${water}), #000)`,
+		boxShadow: `
+			 	inset 2px 2px 12px 2px rgba(${medicine + 50}, ${food + 50}, ${water + 50}, 0.678),
+			1px 1px 10px 1px #222
+		`
 	}
 	
 	return (
